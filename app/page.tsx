9785a5e4d4f8658759126ea4842b62770b2f2a38
Kero2004-A <kerolosakmal@gmail.com>
2026-09-15 09:14:29 +0300

    Add CompanionCard, CompanionList, and CTA components; update layout and utility functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import Cta from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome to my saas app</h1>
+      <h1 className="text-2xl font-bold">Popular Companions</h1>
+  <section className="home-section">
+    <CompanionCard
+    id="1"
+    name="Neura the Brainy Explorer"
+    topic="Neural Network of the Brain"
+    subject="Science"
+    duration={45}
+    color="#ffda6e"
+    />
+    <CompanionCard
+    id="2"
+    name="Neura the Brainy Explorer"
+    topic="Neural Network of the Brain"
+    subject="Science"
+    duration={45}
+    color="#e5d0ff"
+    />
+      <CompanionCard
+    id="3"
+    name="Neura the Brainy Explorer"
+    topic="Neural Network of the Brain"
+    subject="Science"
+    duration={45}
+    color="#bde7ff"
+    />
+  </section>
+    <section className="home-section">
+   <CompanionList 
+   title="Recent Sessions"
+   companions={recentSessions}
+   className="w-2/3 max-lg:w-full"
+   />
+   <Cta/>
+  </section>
     </div>
   )
 }
