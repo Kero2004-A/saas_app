b8daa4c4a8a10a2c68040e260a92f4855681f90b
Kero2004-A <kerolosakmal@gmail.com>
2026-09-16 09:39:16 +0300

    feat: add subscription page with PricingTable component

---
+++ app/subscription/page.tsx
@@ -0,0 +1,12 @@
+import {PricingTable} from "@clerk/nextjs";
+const subscription = () => {
+  return (
+    <main className="flex items-center justify-center h-screen">
+
+      <PricingTable />
+      
+    </main>
+  )
+}
+
+export default subscription
