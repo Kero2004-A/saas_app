34f4f49725c65a57e3fc500334c63fe60d3846f4
Kero2004-A <kerolosakmal@gmail.com>
2026-09-16 08:48:19 +0300

    integrate Clerk for authentication and update layout components

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import {SignIn,SignUp} from '@clerk/nextjs'
+
+export default function SignInPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+
+   <SignIn />
+    </div>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,35 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Navitems from './Navitems'
+import { SignInButton, Show, UserButton, SignUpButton } from '@clerk/nextjs'
 const Navbar = () => {
   return (
-   <nav className="navbar">
-<Link href="/">
-<div className="flex items-center gap-2.5 cursor-pointer">
-    <Image src="/images/logo.svg" alt="Logo"
-     width={46} height={44} />
- 
-</div>
-</Link>
-<div className="flex items-center gap-8">
-    <Navitems />
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="Logo"
+            width={46} height={44} />
 
-  </div>
-   </nav>
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <Navitems />
+
+        <Show when="signed-out">
+          <SignInButton>
+            <button className=" btn-signin">Sign In</button>
+          </SignInButton>
+          <SignUpButton>
+            <button className=" btn-signup">Sign Up</button>
+          </SignUpButton>
+        </Show>
+
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+
+      </div>
+    </nav>
   )
 }
 
