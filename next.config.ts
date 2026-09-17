5af8614fdd99b6ab92547c433b72f3c58893788d
Kero2004-A <kerolosakmal@gmail.com>
2026-09-17 10:24:52 +0300

    feat: implement CompanionSession page with user authentication and companion details display feat: add vapi SDK integration for real-time communication in CompanionComponent chore: update package.json and package-lock.json to include @vapi-ai/web dependency

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imag.clerk.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
